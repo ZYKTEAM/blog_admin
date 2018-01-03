1285e014e6be5dd470a78e5fe7113160bf539812
11641 <11641@MSI>
2018-01-03 16:39:17 +0800

    添加druid

--- src/main/java/com/myblog/Application.java
+++ src/main/java/com/myblog/Application.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.myblog.config.CommonPropertyConfiguration;
 
+
+@ServletComponentScan	//druid连接池
+@EnableTransactionManagement	//事物
 @SpringBootApplication
 @EnableConfigurationProperties(CommonPropertyConfiguration.class)
 public class Application extends SpringBootServletInitializer {
